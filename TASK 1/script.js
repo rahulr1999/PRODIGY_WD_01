42b532e163f77707f466dd969224600d9936febe
rahulr1999 <rahulsince642004@gmail.com>
2024-08-20 13:01:51 +0530

    uploaded a file

---
+++ TASK 1/script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
